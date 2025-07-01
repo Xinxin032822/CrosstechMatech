419ea1dfceed254a5fe29a21203e5feb3a280c01
Xinxin032822 <xinze0009@gmail.com>
2025-07-01 17:24:25 +0800

    add product admin page (safety push)

--- src/Component/ProductForm/ProductForm.jsx
+++ src/Component/ProductForm/ProductForm.jsx
@@ -76,86 +76,89 @@ const ProductForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="row">
-            <div className="field">
-            <label htmlFor="productName">Product Name</label>
-            <Controller
-                name="productName"
-                control={control}
-                render={({ field }) => <input {...field} id="productName" />}
-            />
-            {errors.productName && <p className="error">{errors.productName.message}</p>}
-            </div>
-            <div className="field">
-            <label htmlFor="category">Category</label>
-            <Controller
-                name="category"
-                control={control}
-                render={({ field }) => <input {...field} id="category" />}
-            />
-            {errors.category && <p className="error">{errors.category.message}</p>}
-            </div>
-        </div>
+        <div className='FormAddProductAdminPageMain'>
+            <p className='AdminPageContentAddProductComponentTitle'>Add New Product</p>
+            <form onSubmit={handleSubmit(onSubmit)} className=''>
+                <div className="row">
+                    <div className="field">
+                    <label htmlFor="productName">Product Name</label>
+                    <Controller
+                        name="productName"
+                        control={control}
+                        render={({ field }) => <input className='InputStyleAddproduct' {...field} id="productName" />}
+                    />
+                    {errors.productName && <p className="error">{errors.productName.message}</p>}
+                    </div>
+                    <div className="field">
+                    <label htmlFor="category">Category</label>
+                    <Controller
+                        name="category"
+                        control={control}
+                        render={({ field }) => <input className='InputStyleAddproduct' {...field} id="category" />}
+                    />
+                    {errors.category && <p className="error">{errors.category.message}</p>}
+                    </div>
+                </div>
 
-        <div className="row">
-            <div className="field">
-            <label htmlFor="price">Price</label>
-            <Controller
-                name="price"
-                control={control}
-                render={({ field }) => <input {...field} type="number" id="price" />}
-            />
-            {errors.price && <p className="error">{errors.price.message}</p>}
-            </div>
-            <div className="field">
-            <label htmlFor="image">Image Upload</label>
-            <Controller
-            name="image"
-            control={control}
-            render={({ field }) => (
-                <input
-                type="file"
-                id="image"
-                onChange={(e) => field.onChange(e.target.files)}
-                />
-            )}
-            />
-            {errors.image && <p className="error">{errors.image.message}</p>}
-            </div>
-        </div>
+                <div className="row">
+                    <div className="field">
+                    <label htmlFor="price">Price</label>
+                    <Controller
+                        name="price"
+                        control={control}
+                        render={({ field }) => <input className='InputStyleAddproduct' {...field} type="number" id="price" placeholder='₱'/>}
+                    />
+                    {errors.price && <p className="error">{errors.price.message}</p>}
+                    </div>
+                    <div className="field">
+                    <label htmlFor="image">Image Upload</label>
+                    <Controller
+                        name="image"
+                        control={control}
+                        render={({ field }) => (
+                        <input className='InputStyleAddproduct'
+                        type="file"
+                        id="image"
+                        onChange={(e) => field.onChange(e.target.files)}
+                        />
+                        )}
+                    />
+                    {errors.image && <p className="error">{errors.image.message}</p>}
+                    </div>
+                </div>
 
-        <div className="row">
-            <div className="field full-width">
-            <label htmlFor="description">Description</label>
-            <Controller
-                name="description"
-                control={control}
-                render={({ field }) => <textarea {...field} id="description" />}
-            />
-            {errors.description && <p className="error">{errors.description.message}</p>}
-            </div>
-        </div>
+                <div className="row">
+                    <div className="field full-width">
+                    <label htmlFor="description">Description</label>
+                    <Controller
+                        name="description"
+                        control={control}
+                        render={({ field }) => <textarea className='InputStyleAddproduct' {...field} id="description" />}
+                    />
+                    {errors.description && <p className="error">{errors.description.message}</p>}
+                    </div>
+                </div>
 
-        <div className="row">
-            <div className="field full-width">
-            <label htmlFor="specification">Specification</label>
-            <Controller
-                name="specification"
-                control={control}
-                render={({ field }) => <textarea {...field} id="specification" />}
-            />
-            {errors.specification && <p className="error">{errors.specification.message}</p>}
-            </div>
-        </div>
+                <div className="row">
+                    <div className="field full-width">
+                    <label htmlFor="specification">Specification</label>
+                    <Controller
+                        name="specification"
+                        control={control}
+                        render={({ field }) => <textarea className='InputStyleAddproduct' {...field} id="specification" />}
+                    />
+                    {errors.specification && <p className="error">{errors.specification.message}</p>}
+                    </div>
+                </div>
 
-        <div className="row">
-            <button type="submit" className='btnSubmitSaveProduct'>
-                <svg className='svgSaveProductButton' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="floppy-disk" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="#fff" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 288a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"></path></svg>
-                Save Product
-            </button>
+                <div className="row">
+                    <button type="submit" className='btnSubmitSaveProduct'>
+                        <svg className='svgSaveProductButton' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="floppy-disk" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="#fff" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 288a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"></path></svg>
+                        Save Product
+                    </button>
+                </div>
+            </form>
         </div>
-        </form>
     );
 };
 
--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -7,8 +7,7 @@ import '../Styles/Admin.css';
 import { useState } from 'react';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getDocs, collection } from 'firebase/firestore';
+import { getFirestore, getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
 import ProductForm from '../Component/ProductForm/ProductForm';
 const firebaseConfig = {
   apiKey: "AIzaSyA1SaxJky2fCYkbyUDF1lfsCPROPo71-C0",
@@ -23,8 +22,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
 function Admin() {
+    const [activeNav, setActiveNav] = useState("Product Management");
     const [products, setProducts] = useState([]);
     const fetchProducts = async () => {
         const querySnapshot = await getDocs(collection(db, "products"));
@@ -38,65 +37,92 @@ function Admin() {
     useEffect(() => {
         fetchProducts();
     }, []);
+
+    const handleDelete = async (id) => {
+    try {
+        await deleteDoc(doc(db, "products", id));
+        setProducts(products.filter(p => p.id !== id));
+    } catch (error) {
+        console.error("Error deleting product:", error);
+    }
+    };
+
   return (
-    <div>
-        
+    <div className='MainDivAdminPage'>
         <div>
-            <p>Admin Panel</p>
-            <p>Manage products and customer inquiries</p>
+            <p className='AdminPanelTextMainPage'>Admin Panel</p>
+            <p className='ManageProductsTextMainPage'>Manage products and customer inquiries</p>
         </div>
-        <div>
+        <div className='MainNavAdminPage'>
             <ul className='ulAdminPage'>
-                <div className='ulChildDivAdminPage'>
+                <div className={`ulChildDivAdminPage ${activeNav === 'Product Management' ? 'active' : ''}`}
+                    onClick={() => setActiveNav('Product Management')}>
                     <svg className='SVGAdminPage' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="gears" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M308.5 135.3c7.1-6.3 9.9-16.2 6.2-25c-2.3-5.3-4.8-10.5-7.6-15.5L304 89.4c-3-5-6.3-9.9-9.8-14.6c-5.7-7.6-15.7-10.1-24.7-7.1l-28.2 9.3c-10.7-8.8-23-16-36.2-20.9L199 27.1c-1.9-9.3-9.1-16.7-18.5-17.8C173.9 8.4 167.2 8 160.4 8h-.7c-6.8 0-13.5 .4-20.1 1.2c-9.4 1.1-16.6 8.6-18.5 17.8L115 56.1c-13.3 5-25.5 12.1-36.2 20.9L50.5 67.8c-9-3-19-.5-24.7 7.1c-3.5 4.7-6.8 9.6-9.9 14.6l-3 5.3c-2.8 5-5.3 10.2-7.6 15.6c-3.7 8.7-.9 18.6 6.2 25l22.2 19.8C32.6 161.9 32 168.9 32 176s.6 14.1 1.7 20.9L11.5 216.7c-7.1 6.3-9.9 16.2-6.2 25c2.3 5.3 4.8 10.5 7.6 15.6l3 5.2c3 5.1 6.3 9.9 9.9 14.6c5.7 7.6 15.7 10.1 24.7 7.1l28.2-9.3c10.7 8.8 23 16 36.2 20.9l6.1 29.1c1.9 9.3 9.1 16.7 18.5 17.8c6.7 .8 13.5 1.2 20.4 1.2s13.7-.4 20.4-1.2c9.4-1.1 16.6-8.6 18.5-17.8l6.1-29.1c13.3-5 25.5-12.1 36.2-20.9l28.2 9.3c9 3 19 .5 24.7-7.1c3.5-4.7 6.8-9.5 9.8-14.6l3.1-5.4c2.8-5 5.3-10.2 7.6-15.5c3.7-8.7 .9-18.6-6.2-25l-22.2-19.8c1.1-6.8 1.7-13.8 1.7-20.9s-.6-14.1-1.7-20.9l22.2-19.8zM112 176a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM504.7 500.5c6.3 7.1 16.2 9.9 25 6.2c5.3-2.3 10.5-4.8 15.5-7.6l5.4-3.1c5-3 9.9-6.3 14.6-9.8c7.6-5.7 10.1-15.7 7.1-24.7l-9.3-28.2c8.8-10.7 16-23 20.9-36.2l29.1-6.1c9.3-1.9 16.7-9.1 17.8-18.5c.8-6.7 1.2-13.5 1.2-20.4s-.4-13.7-1.2-20.4c-1.1-9.4-8.6-16.6-17.8-18.5L583.9 307c-5-13.3-12.1-25.5-20.9-36.2l9.3-28.2c3-9 .5-19-7.1-24.7c-4.7-3.5-9.6-6.8-14.6-9.9l-5.3-3c-5-2.8-10.2-5.3-15.6-7.6c-8.7-3.7-18.6-.9-25 6.2l-19.8 22.2c-6.8-1.1-13.8-1.7-20.9-1.7s-14.1 .6-20.9 1.7l-19.8-22.2c-6.3-7.1-16.2-9.9-25-6.2c-5.3 2.3-10.5 4.8-15.6 7.6l-5.2 3c-5.1 3-9.9 6.3-14.6 9.9c-7.6 5.7-10.1 15.7-7.1 24.7l9.3 28.2c-8.8 10.7-16 23-20.9 36.2L315.1 313c-9.3 1.9-16.7 9.1-17.8 18.5c-.8 6.7-1.2 13.5-1.2 20.4s.4 13.7 1.2 20.4c1.1 9.4 8.6 16.6 17.8 18.5l29.1 6.1c5 13.3 12.1 25.5 20.9 36.2l-9.3 28.2c-3 9-.5 19 7.1 24.7c4.7 3.5 9.5 6.8 14.6 9.8l5.4 3.1c5 2.8 10.2 5.3 15.5 7.6c8.7 3.7 18.6 .9 25-6.2l19.8-22.2c6.8 1.1 13.8 1.7 20.9 1.7s14.1-.6 20.9-1.7l19.8 22.2zM464 304a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
                     <li className='LiUlDivAdminPageMainNav'>Product Management</li>
                 </div>
-                <div className='ulChildDivAdminPage ulChildDivAdminPageStyle'>
+                <div    className={`ulChildDivAdminPage ulChildDivAdminPageStyle ${activeNav === 'Inquiry Management' ? 'active' : ''}`}
+                        onClick={() => setActiveNav('Inquiry Management')}>
                     <svg className='SVGAdminPage' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path></svg>
                     <li className='LiUlDivAdminPageMainNav'>Inquiry Management</li>
                 </div>
-                <div className='ulChildDivAdminPage '>
-                    <img className='SVGAdminPage' src="src/assets/img/shipping-fast.svg" alt="" />
+                <div    className={`ulChildDivAdminPage ${activeNav === 'Delivery Management' ? 'active' : ''}`}
+                        onClick={() => setActiveNav('Delivery Management')}>
+                    <svg className='SVGAdminPage' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="truck" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
                     <li className='LiUlDivAdminPageMainNav'>Delivery Management</li>
                 </div>
             </ul>
         </div>
         <div>
-            <div>
-                <p>Add New Product</p>
-                <ProductForm />
-                <div>
-                    <p>Current Products</p>
-                    <table className='tableAdminPage'>
-                        <thead>
-                            <tr>
-                                <th>Product Name</th>
-                                <th>Category</th>
-                                <th>Price</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+            { activeNav === 'Product Management' && (
+                <div className='AdminPageContentAddProductComponent'>
+                    <ProductForm />
+                    <div className='AdminPageContentAddProductComponentTable'>
+                        <p className='CurrentProductsAdminPage'>Current Products</p>
+                        <table className='tableAdminPage'>
+                            <thead className='theadAdminPage'>
+                                <tr>
+                                    <th className='theadLabelAdminPage'>Product</th>
+                                    <th className='theadLabelAdminPage'>Category</th>
+                                    <th className='theadLabelAdminPage'>Price</th>
+                                    <th className='theadLabelAdminPage'>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
                             {products.map(product => (
                                 <tr key={product.id}>
-                                    <td>{product.productName}</td>
-                                    <td>{product.category}</td>
-                                    <td>${product.price}</td>
-                                    <td>
-                                        {product.imageName && (
-                                            <img 
-                                                src={product.imageName} 
-                                                alt={product.imageName} 
-                                                style={{ width: '50px', height: 'auto' }}
-                                            />
-                                        )}
-                                    </td>
+                                <td style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                                    {product.imageName && (
+                                    <img 
+                                        src={product.imageName} 
+                                        alt={product.productName} 
+                                        style={{ width: '50px', height: 'auto', borderRadius: '4px' }}
+                                    />
+                                    )}
+                                    <span>{product.productName}</span>
+                                </td>
+                                <td>{product.category}</td>
+                                <td>₱{product.price}</td>
+                                <td>
+                                    <button 
+                                    className="editBtn" 
+                                    onClick={() => handleEdit(product)}
+                                    >
+                                    Edit
+                                    </button>
+                                    <button 
+                                    className="deleteBtn" 
+                                    onClick={() => handleDelete(product.id)}
+                                    >
+                                    Delete
+                                    </button>
+                                </td>
                                 </tr>
                             ))}
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
 
     </div>
